936ca1b466477448a36ecc653d3648eb42d03c1d
CherriPepsi <84200658+CherriPepsi@users.noreply.github.com>
2021-06-16 19:36:48 -0400

    This is the code for lab one. Feel free to make changes to the lab, as I was supposed to design the form.
    Code for lab 1.  Javascript, React, Material UI. Collabs, Lab One, Team 06.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,79 @@
+import React from 'react'
 import Header from "./component/header"
-import './App.css';
+import './App.css'
+import './component/header/header.css'
+import ToDoList from './component/todo/todolist'
 
-function App() {
+
+
+function Task ({ task, index, completedTask }) {  //Gather Task list and Time submitted
   return (
+    <div className='task'
+        style={{textDecoration: task.isDone ? 'line-through' : ''}}
+    > 
+    {task.text}
+    <br/>
+    {task.time = new Date().toLocaleDateString() + ' ' + ' ' + new Date().toLocaleTimeString()}
+    <div> 
+     <button onClick={() => completedTask(index)} className= 'check'>✓</button>
+     <button onClick={() => ''} className= 'edit'>↻</button> 
+     <button onClick={() => ''} className= 'delete'>✕</button>
+    </div>
+    </div>
+ 
+   ); //Add Delete and Edit buttons under the 'check' button.
+  };
+
+function App() {  //Display the Tasklist
+const [tasks, setTasks] = React.useState([ 
+  {
+       text: 'Go grocery shopping',
+       isDone: false
+  },
+
+  {
+       text: 'Feed the pet',
+       isDone: false
+  },
+
+  {
+       text: 'Do the laundry',
+       isDone: false
+  }
+
+]);
+
+  const addTask = text => {     //Allows the user to add an task
+    const newTasks = [...tasks, {text}];
+    setTasks(newTasks);
+  };
+
+  const completedTask = index => {   //Allows user to mark task as 'complete'
+    const newTasks = [...tasks];
+    newTasks[index].isDone = true;
+    setTasks(newTasks);
+  };
+
+    // Add code for the Deleted Task here.
+
+   // Add Code for the Updated Task Here.
+
+  return ( // Allows user interact with the form
     <div className="App">
     <Header/>
+    <ToDoList addTask = {addTask}/> 
+      <div className="task-list">
+      {tasks.map((task, index) => (
+        <Task
+        key={index}
+        index = {index}
+        task = {task}
+        time = {task.time}
+        completedTask = {completedTask}
+       //Add deleted task property here
+        />
+      ))}
+    </div>
     </div>
   );
 }
---
+++ src/component/todo/todolist.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+function ToDoForm({ addTask })
+{
+  const [value, setValue] = React.useState(''); // Don't forget to validate duplicate tasks.
+
+    const handleSubmit = e => {
+      e.preventDefault();
+      if (!value) return;
+      addTask(value);
+      setValue('');
+    };
+  
+    return (
+
+        <form onSubmit={handleSubmit}>
+ 
+        <TextField id="outlined-basic" label="Task" variant="filled" type="text" className="input" 
+        value={value} onChange={e => 
+        setValue(e.target.value)}/> 
+        
+        <Button variant="contained" type='submit' className='button' style={{background: '#b30000', 
+        color: 'white'}}>
+          Add
+        </Button>
+        </form> //Add button to be coded, in order to add tasks to the form
+    );
+} 
+
+export default ToDoForm;
