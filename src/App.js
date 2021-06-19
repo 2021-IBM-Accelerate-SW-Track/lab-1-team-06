9ec8a636a3a9c7ba218ae47f4e94daae7f03ad9b
CherriPepsi <84200658+CherriPepsi@users.noreply.github.com>
2021-06-18 20:00:35 -0400

    Update
    Submitted design for the ToDo List for Lab One, Team 06. Change is Compromised.

--- src/App.js
+++ src/App.js
@@ -4,66 +4,12 @@ import './App.css'
 import './component/header/header.css'
 import ToDoForm from './component/todo/todoform'
 
-function TaskList({ task, index, completedTask }) //Allows the task, date, time, 
-//and buttons to display. Add edit and delete task code to the parameter. 
-//Time displays at the same time, so it needs another format or coded differently, 
-//so it could display at different times?
-{
-    return ( 
-        <div className = 'task' // Marks task as 'Complete'. 'Edit' and 'Delete' buttons to be edited. 
-        > 
-        <span style={{ textDecoration: task.isCompleted ? 'line-through' : '' }}> { task.text }
-          <br/> 
-          { task.time = new Date().toLocaleDateString() + ' ' + new Date().toLocaleTimeString()}</span>
-          <div>
-          <button onClick={() => completedTask(index)} className= 'check'>✓</button> {''}
-          <button onClick={() => ''} className= 'edit'>✎</button> 
-          <button onClick={() => ''} className= 'delete'>✕</button> 
-          </div>
-       </div>
-     );
- };
 
 function App() { 
-  const [tasks, setTasks] = React.useState([
-    { text: 'Go grocery shopping' , isCompleted: false}, //Helps display the task list to the form
-    { text: 'Feed the pets', isCompleted: false},        //Marks task if task is complete (boolean = true)
-    { text: 'Do the laundry', isCompleted: false}
-  ]);
-
-    const addTask = text => { //Allows the user to add tasks. 
-      //The 'addTask' code needs to be fixed. Jason/team can run and see what it does.
-    const newTasks = [...tasks, { text }];
-    setTasks(newTasks);
-    };
-
-    const completedTask = index => {
-    const newTasks = [...tasks];
-    newTasks[index].isCompleted = true;
-    setTasks(newTasks);
-    };
-
-    //Edit task code goes here
-
-    //Delete task code goes here
-
   return ( 
     <div className="App">
     <Header/> 
-    <ToDoForm addTask={addTask} />    
-    <div className='task-list'>
-        {tasks.map((task, index) => (
-           <TaskList // Task properties
-             key={index}
-             index={index}
-             task={task}
-             time={task.time}
-             completedTask = {completedTask}
-             //edit = {edittask} property here
-             //deleted task property here
-             />
-        ))}
-    </div>
+    <ToDoForm/>    
     </div>
 );
 }
--- src/component/todo/todoform.js
+++ src/component/todo/todoform.js
@@ -2,32 +2,23 @@ import React from 'react'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
- function ToDoForm({ addTask })  //The Validation code goes in this function 
- //or wherever you want to put it
+ function ToDoForm()  
  {
-   const [value, setValue] = React.useState(''); // This creates the Add property for the task
-
-   const handleSubmit = e => {
-     e.preventDefault();
-
-     if (!value) return;
-     addTask(value);
-     setValue('');
-   };
 
    return (
-      <form onSubmit={handleSubmit}> 
+      <form> 
          
-      <TextField id="outlined-basic" label="Task" variant="filled" //Allows the user to type a task
-      data-testid='new-item-input' className="input" type='text' value={value} 
-      onChange={e => setValue(e.target.value)} /> 
+      <TextField id="outlined-basic" label="Task" variant="filled" //INPUT FIELD AND BUTTON
+      data-testid='new-item-input' className="input" type='text'/> 
         
       <Button variant="contained" className='button' type='submit' data-testid='new-item-button'  
       style={{background: '#b30000', color: 'white' }}> 
          Add
       </Button>
         
-      </form> //The Button allows user to submit to the form
+      </form> //YOU CAN CHANGE THE COLOR OF THE TASKS IF YOU WANT, BUT BACKGROUND COLOR, HEADER, 
+      //INPUT AND BUTTON COLOR STAYS, LOL!!! AND DON'T FORGET TO ADJUST THE FORM, SO IT CAN STAY IN PLACE,
+      // WHILE MINIMIZED! YOU'RE FREE TO MOVE IT AROUND - RYNISHA
     );
   }
 
